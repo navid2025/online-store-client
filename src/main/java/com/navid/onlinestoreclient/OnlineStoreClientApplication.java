ac528bd6708b8d102cd74c02c5de6e9b65cea9e0
n.zare <n.zare@behsacorp.com>
2024-02-10 15:31:08 +0330

    first commit

---
+++ src/main/java/com/navid/onlinestoreclient/OnlineStoreClientApplication.java
@@ -0,0 +1,13 @@
+package com.navid.onlinestoreclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineStoreClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/navid/onlinestoreclient/OnlineStoreClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navid.onlinestoreclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
